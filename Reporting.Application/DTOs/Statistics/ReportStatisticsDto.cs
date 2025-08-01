4759b474944ab8438271d06f40c5dd1fd6050a62
Volkan Arslan <volkanarslan@protonmail.com>
2025-08-01 10:32:02 +0300

    Update report dto and consumer service, Add migration for reportstatistics field

---
+++ Reporting.Application/DTOs/Statistics/ReportStatisticsDto.cs
@@ -0,0 +1,9 @@
+namespace Reporting.Application.DTOs.Statistics;
+
+public class ReportStatisticsDto
+{
+    public string Location { get; set; }
+    public int PersonCount { get; set; }
+    public int PhoneNumberCount { get; set; }
+    public int EmailCount { get; set; }
+}
--- Reporting.Infrastructure/Migrations/20250801065853_Add_ReportStatisticsFields.Designer.cs
+++ Reporting.Infrastructure/Migrations/20250801065853_Add_ReportStatisticsFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Reporting.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Reporting.Infrastructure.Persistence;
 namespace Reporting.Infrastructure.Migrations
 {
     [DbContext(typeof(ReportingDbContext))]
-    partial class ReportingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250801065853_Add_ReportStatisticsFields")]
+    partial class Add_ReportStatisticsFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
